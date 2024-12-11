88ecd305bf7244cea00250629e5ef0a24d143ad1
dorian68 <labrydorian.mpsi@hotmail.fr>
2024-12-11 21:05:03 +0100

    eeee

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home2, name='home2'),
-    path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('serviceFirst/', views.serviceFirst, name='serviceFirst'),
     path('serviceSecond/', views.serviceSecond, name='serviceSecond'),
@@ -14,4 +13,6 @@ urlpatterns = [
     path('corpoFinance/', views.corpoFinance, name='corpoFinance'),   
     path('ratesAnalysis/',views.ratesAnalysis, name='ratesAnalysis'),
     path('meanReversion/',views.meanReversion, name='meanReversion'),
+    path('about/', views.about, name='about'),
 ]
+
